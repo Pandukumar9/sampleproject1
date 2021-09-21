33cd5495f01bb06a06224d1a9dd5db7270e0a6f4
pandukumar9 <pandukumar.gujja@gmail.com>
2021-09-21 09:26:35 +0530

    new code

--- src/app/forms/reativeforming/reativeforming.component.ts
+++ src/app/forms/reativeforming/reativeforming.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ConfirmPasswordValidator } from './confirmpasswordvalidator';
+
 
 @Component({
   selector: 'app-reativeforming',
@@ -8,19 +10,49 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class ReativeformingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _myFB : FormBuilder) { }
 
+  // registrationForm = new FormGroup({
+  //   username: new FormControl('PANDU'),
+  //   password: new FormControl(''),
+  //   address: new FormGroup({
+  //     city: new FormControl(''),
+  //     state: new FormControl('')
+  //   })
+  // });
+  get Username(){
+    return this.registrationForm.get('username')
+  }
+  registrationForm= this._myFB.group(
+    {
+    username: ['',Validators.required],
+    password: ['',Validators.required],
+    confirmPassword: ["",Validators.required],
+    
+    address :this._myFB.group({
+      city: [''],
+      state: ['']
+    },
+      {
+        validator: ConfirmPasswordValidator("password", "confirmPassword")
+      }
+    )
+  });
   ngOnInit(): void {
   }
-
-  registrationForm = new FormGroup({
-    firstname: new FormControl(''),
-    lastname: new FormControl('')
-  });
-
+  selectedValues(){
+    this.registrationForm.patchValue({  //instead of setValue we can use patchValue
+      username: 'Pandkumar',  
+      address: {
+        city: 'warangal',
+        state: 'Telangana'
+    }
+  })
+  }
+  submitted: boolean = true;
   onSubmited(registrationForm){
     console.log(registrationForm);
-    alert(registrationForm)
+    this.submitted=true;
   }
 
 }
